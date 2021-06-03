62715ba5c92fb34d5ca6eb09455565e4ea8b73e8
trinidad de achaval <trinideachaval@gmail.com>
2021-06-03 17:28:52 +0200

    bookmark almost finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'list#index'
+  root to: 'lists#index'
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
